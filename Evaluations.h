9a378c309a912c58260410a365741d0ced01dd0f
AlexPlancon <62606492+AlexPlancon@users.noreply.github.com>
2020-04-02 18:03:56 +0200

    Add files via upload

---
+++ Evaluations.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef EVALUATIONS_H_
+#define EVALUATIONS_H_
+
+#include<opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/core.hpp>
+#include<iostream>
+#include <vector>
+
+std::vector<float> secondMethode(cv::Mat prediction, cv::Mat verite_terrain);
+
+void displayMatriceConf(std::vector<float> matriceConfusion);
+
+std::vector<float>  displayScore(std::vector<float> matriceConfusion);
+
+#endif // !EVALUATIONS_H_
---
+++ ImageProcessing.h
@@ -0,0 +1,37 @@
+#pragma once
+#ifndef IMAGEPROCESSING_H_
+#define IMAGEPROCESSING_H_
+
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+#include <string>
+
+void recomb(cv::Mat& src, cv::Mat& dst);
+
+void forwardFFT(cv::Mat& Src, cv::Mat* FImg);
+
+void forwardFFT_Mag_Phase(cv::Mat& src, cv::Mat& Mag, cv::Mat& Phase);
+
+void inverseFFT(cv::Mat* FImg, cv::Mat& Dst);
+
+void inverseFFT_Mag_Phase(cv::Mat& Mag, cv::Mat& Phase, cv::Mat& dst);
+
+void highPassFilter(cv::Mat& Mag, cv::Mat& Phase);
+
+void filterDirectional(cv::Mat& Mag, cv::Mat& Phase);
+
+void tfOutput(cv::Mat& Mag, cv::Mat& Phase, cv::Mat& img, std::string path);
+
+void lectureImage(cv::Mat& img, std::string path, std::string name, int& rows, int& cols);
+
+void greyscaleImage(cv::Mat& img, std::string path);
+
+void equlization(cv::Mat& img, std::string path);
+
+void transformFourier(cv::Mat& img, std::string path);
+
+void binarization(cv::Mat& img, std::string path, int rows, int cols);
+
+#endif // !IMAGEPROCESSING_H_
